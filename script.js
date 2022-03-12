1cec3421cc1c5f46d1f479781a1d8385eebde51b
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-13 02:53:01 +0300

    Add jquery function to portfolio section5

--- script.js
+++ script.js
@@ -37,3 +37,43 @@ $(document).ready(function(){
         })
     })
 });
+
+// container 1
+$(document).ready(function(){
+    $("#image4").hover(function(){
+        $("#image4").css("filter","brightness(50%)");
+        $("#div4").toggle();
+    });
+    $("#image3").hover(function(){
+        $("#image3").css("filter","brightness(50%)");
+        $("#div3").toggle();
+    });
+    $("#image2").hover(function(){
+        $("#image2").css("filter","brightness(50%)");
+        $("#div2").toggle();
+    });
+    $("#image1").hover(function(){
+        $("#image1").css("filter","brightness(50%)");
+        $("#div1").toggle();
+    });
+});
+
+// container 2
+$(document).ready(function(){
+    $("#image5").hover(function(){
+        $("#image5").css("filter","brightness(50%)");
+        $("#div5").toggle();
+    });
+    $("#image6").hover(function(){
+        $("#image6").css("filter","brightness(50%)");
+        $("#div6").toggle();
+    });
+    $("#image7").hover(function(){
+        $("#image7").css("filter","brightness(50%)");
+        $("#div7").toggle();
+    });
+    $("#image8").hover(function(){
+        $("#image8").css("filter","brightness(50%)");
+        $("#div8").toggle();
+    });
+});
