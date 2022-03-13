6b7cce54eabb78450284ae1a24238da4e0fec5f1
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-13 18:45:46 +0300

    ft: Fix bugs, add media queries

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
 // Jquery functions
 // design section
 $(document).ready(function(){
-    $(".design-image").click(function(){
-      $(".design-image").toggle(1000);
-      $(".design").show(1000);
-    });
     $(".design").click(function(){
-      $(".design").slideUp(1000);
-      $(".design-image").slideDown(1000);
+      $(".design-image").slideToggle(1000);
+      $(".text1").slideToggle(1000);
     });
   });
 
 // development section
 $(document).ready(function(){
-    $(".dev-image").click(function(){
-        $(".dev-image").toggle(1000);
-        $(".development").show(1000);
-
-        $(".development").click(function(){
-            $(".development").slideUp(1000);
-            $(".dev-image").slideDown(1000);
-        })
+    $(".development").click(function(){
+        $(".dev-image").slideToggle(1000);
+        $(".text2").slideToggle(1000);
     });
 });
 
 
 // product management
 $(document).ready(function(){
-    $(".product-image").click(function(){
+    $(".product").click(function(){
         $(".product-image").toggle(1000);
-        $(".productM").show(1000);
-
-        $(".productM").click(function(){
-            $(".productM").slideUp(1000);
-            $(".product-image").slideDown(1000);
-        })
-    })
+        $(".text3").show(1000);
+    });
 });
 
 // container 1
