2b2fdaadddff2a2d2d07b6860ebdd0d52dc291ac
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-13 04:35:40 +0300

    Add function to form

--- script.js
+++ script.js
@@ -77,3 +77,21 @@ $(document).ready(function(){
         $("#div8").toggle();
     });
 });
+
+// javascript form
+function send(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if(name.length == ""){
+        alert("please enter name");
+    }else if(email == ""){
+        alert("invalid email!!");
+    }else if(message == ""){
+        alert("Please enter message");
+    }else if(name && email == true){
+    }else{
+        alert(name + " " + "We have received your message. Thank you for reaching out to us.");
+    };
+}
